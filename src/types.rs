268a31010f068a1b26b7c6b790ed6ed63eb51dee
Syfaro <syfaro@huefox.com>
2021-05-20 23:24:45 -0400

    Add some fields to Chat.

--- src/types.rs
+++ src/types.rs
@@ -89,13 +89,16 @@ pub struct Chat {
     pub username: Option<String>,
     pub first_name: Option<String>,
     pub last_name: Option<String>,
+    pub bio: Option<String>,
     pub description: Option<String>,
     pub invite_link: Option<String>,
     pub pinned_message: Option<Box<Message>>,
     pub permissions: Option<ChatPermissions>,
     pub slow_mode_delay: Option<i32>,
+    pub message_auto_delete_time: Option<i32>,
     pub sticker_set_name: Option<String>,
     pub can_set_sticker_set: Option<bool>,
+    pub linked_chat_id: Option<i64>,
 }
 
 #[derive(Clone, Debug, Deserialize, Default, Serialize)]
